1f99c3f18b3e5c3fa0861d4159cd5b8e3878c9f0
LonnieWebb <lwebb40@gatech.edu>
2024-05-01 12:43:22 -0400

    Remove deprecated functinos

--- include/basematerial.h
+++ include/basematerial.h
@@ -17,14 +17,4 @@ class BaseMaterial {
       : name(name), type(type), E(E), rho(rho), nu(nu) {}
 
   virtual ~BaseMaterial() {}
-
-  // Pure virtual functions for the derived classes to implement
-  virtual void D_compute_stiffnessMatrix3D(T *output) const = 0;
-  //   virtual void D_compute_stiffnessMatrix2D(T* output) const = 0;
-  virtual void compute_dstrain(const T *gradient_u, int num_integrations,
-                               T *output) const = 0;
-  virtual void compute_stress(const T *dstrain, const T *strain,
-                              T *output) const = 0;
-  virtual void compute_wave_speed3D(T *output) = 0;
-  virtual void compute_wave_speed2D(T *output) = 0;
 };
